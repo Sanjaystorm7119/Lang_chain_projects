708e5e362b4cf2f6aef1760d1f7d9b19a1312182
Sanjay b <sanjaysrivastavab@gmail.com>
2026-03-08 00:10:39 +0530

    fix deprecated libraries

--- withuv/05RAG_groq.py
+++ withuv/05RAG_groq.py
@@ -4,10 +4,10 @@ from langchain_groq import ChatGroq
 from langchain_openai import OpenAIEmbeddings
 from langchain_ollama import OllamaEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.chains.combine_documents import create_stuff_documents_chain
 
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import create_retrieval_chain
+from langchain_core.chains import create_retrieval_chain
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 import openai
